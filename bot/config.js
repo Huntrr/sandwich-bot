0d2cdf4f74b6cecd6539962274831c9f7114401d
Hunter Lightman <git.huntrr@gmail.com>
2016-01-07 21:22:24 -0500

    Updated config

--- bot/config.js
+++ bot/config.js
@@ -1,8 +1,13 @@
 module.exports = {
   "credentials": {
-    "email": process.env.FB_EMAIL || "test@example.com",
+    "email": process.env.FB_EMAIL || "email@test.com",
     "password": process.env.FB_PASSWORD || "password"
   },
   "commandCharacter": "/",
-  "name": process.env.FB_NAME || "Firstname Lastname"
+  "name": process.env.FB_NAME || "Firstname Lastname",
+  "db": {
+    "url": process.env.DB_URL || "mongo_url"
+  },
+  "owner": 0,
+  "ownerName": "John Smith"
 }
